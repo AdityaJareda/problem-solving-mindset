fab12d8db34eaaf259fd51c63c0a42ed61dde2bf
AdityaJareda <adityajareda@gmail.com>
2025-02-05 23:48:24 +0530

    Task 3: Understood about variables and data types

---
+++ Python Basics/variable_&_datatypes.py
@@ -0,0 +1,39 @@
+# Task 3: Variables and Data Types
+
+this_is_variable = "This is a data assigned to the variable"
+
+Int = 10              # Data Type - Integer
+Str = "Hello"         # Data Type - String
+Float = 10.00           # Data Type - Float
+
+# 1. Variable assignment
+
+college_problem = "Main problem of Indian colleges is that they are profit oriented rather than being education oriented."
+print(college_problem)      # Printing the variable
+print()                     # Blank line
+
+# 2. Understanding Data Types using Arithmatic Operators
+
+a = 10
+b = 3
+c = 10.5
+
+print('Understanding Integer Data Type using Arithmatic Operators\n')
+print('a = ',a)
+print('b = ',b)
+print('c = ',c)
+print()
+print('Addition - ',a+b)        # 10 + 3 = 13 [Integer]
+print('Subtraction - ',a-b)     # 10 - 3 = 7 [Integer]
+print('Division - ', a/b)       # 10 / 3 ≈ 3.333 [Float]
+
+print('\nMixed Addition (Interger + Float) - ',a+c)     # 10 + 10.5 = 20.5 [Float]
+
+'''
+Learning Outcomes:
+                1. Variable - can store and reuse data.
+                2. Data Types:
+                            Int - for whole numbers.
+                            float - for numbers with decimals.
+                            str - for textual data. 
+'''
