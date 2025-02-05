ee4204b92ed9054fd79efb2f22d0d9f1c407ff68
AdityaJareda <adityajareda@gmail.com>
2025-02-05 16:41:02 +0530

    Task 1: Understood Print() function

---
+++ Python Basics/print()_function.py
@@ -0,0 +1,9 @@
+# Task 1: Printing a statement using print()
+
+print('Hello, World!')
+
+''' 
+Learning Outcomes:
+                  1. Learned how to creating a python file and write a program in it.
+                  2. Understood print() function - used for printing text on screen.
+'''
