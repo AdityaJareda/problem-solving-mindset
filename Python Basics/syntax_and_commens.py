dc622cff4afda3e74687c6a51f1aedc0b9f5c4dd
AdityaJareda <adityajareda@gmail.com>
2025-02-05 16:55:13 +0530

    Task 2: Understood Syntax and Comments

---
+++ Python Basics/syntax_and_commens.py
@@ -0,0 +1,16 @@
+# Task 2: Syntax basics & Comments
+
+'''__print("Hello, World!")'''         # Will Produce indentation error
+
+print("\n1. (#) is used for single line comments.")
+
+print("""\n2. ('''Hello
+   World!''')  is used for multiline comments.""")
+
+
+
+'''
+Learning Outcomes:
+                  1. Learned about proper syntax and intentation.
+                  2. Understood about comments, '#' used for single line comments and (''' ''') user for multiline comments [not visible on terminal].
+'''         
